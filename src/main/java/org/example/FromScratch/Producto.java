81c7958a20df163ec77ebb25c6736300864a4303
avidaldo <avd.formacion@gmail.com>
2022-10-03 20:25:56 +0200

    WIP

--- src/main/java/org/example/FromScratch/Producto.java
+++ src/main/java/org/example/FromScratch/Producto.java
@@ -1,38 +1,27 @@
 package org.example.FromScratch;
 
-public class Producto implements ProductoComparable{
+public class Producto implements ProductoComparable {
     private String nombre;
     private Integer precio;
     private int ventas = 0;
 
     public Producto(String nombre, int precio) {
-        this.nombre=nombre;
+        this.nombre = nombre;
         this.precio = precio;
-    }
-
-    public Integer getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Integer precio) {
-        this.precio = precio;
-    }
-
-    public int getVentas() {
-        return ventas;
     }
 
     public void setVentas(int ventas) {
         this.ventas = ventas;
     }
 
-
-
-
+    @Override
+    public String toString() {
+        return nombre + " (" + precio + "€ - " + ventas + " ven.)";
+    }
 
     @Override
     public boolean esMayor(ProductoComparable otroProducto) {
-        return precio>((Producto)otroProducto).precio;
+        return precio > ((Producto) otroProducto).precio;
         //return ventas>((Producto)otroProducto).ventas;
     }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 
 import org.example.FromScratch.Producto;
 
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
 
@@ -13,17 +15,34 @@ class Main {
             System.out.println(val);
         }*/
 
+        Producto[] productos = new Producto[5];
 
-        Producto[] productos = new Producto[]{
-                new Producto("Samsung Galaxy A12", 200),
-                new Producto("Xiaomi Redmi 9 ", 120),
-                new Producto("Xiaomi Redmi 9A", 150)
-        };
+        Producto p1 = new Producto("P1", 500);
+        p1.setVentas(1000);
+        Producto p2 = new Producto("P2", 400);
+        p2.setVentas(2000);
+        Producto p3 = new Producto("P3", 300);
+        p3.setVentas(3000);
+        Producto p4 = new Producto("P4", 200);
+        p4.setVentas(4000);
+        Producto p5 = new Producto("P5", 100);
+        p5.setVentas(5000);
 
+
+        productos[0] = p3;
+        productos[1] = p4;
+        productos[2] = p5;
+        productos[3] = p2;
+        productos[4] = p1;
+
+
+
+
+        System.out.println("Lista antes de ordenar: " + Arrays.toString(productos));
 
         LibreriaOrdenar.ordenarProductos(productos);
 
-        System.out.println(productos);
+        System.out.println("Lista ordenada:         " + Arrays.toString(productos));
 
     }
 
