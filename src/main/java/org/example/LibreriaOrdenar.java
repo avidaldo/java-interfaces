2c0aa64d11a03674da431af1d358cbea52e01675
Alejandro Vidal <avd.formacion@gmail.com>
2022-10-05 12:53:13 +0200

    WIPW

--- src/main/java/org/example/LibreriaOrdenar.java
+++ src/main/java/org/example/LibreriaOrdenar.java
@@ -19,7 +19,7 @@ public final class LibreriaOrdenar {
     public static void ordenarProductos(ProductoComparable[] data) {
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data.length - 1; j++) {
-                if (data[j].esMayor(data[j + 1])) {
+                if (data[j].esMayor(data[j + 1])) {   // Necesitamos los objetos a comparar tengan el método "esMayor()"
                     ProductoComparable temp = data[j];
                     data[j] = data[j + 1];
                     data[j + 1] = temp;
